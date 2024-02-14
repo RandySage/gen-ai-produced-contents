fbae694a4568156917ec0ad8469eb745abdbbd6d
Randy Sage <rsage@thinhorn.com>
2024-02-14 10:57:47 -0800

    Local changes (unknown status)

--- trap_and_print_signals.sh
+++ trap_and_print_signals.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Function to handle signals
-handle_signal() {
-  echo "Caught signal $1" >> /tmp/signals.out
+simulate_response() {
+  for n in $(seq 1 20) ; do
+    echo "$(date +%H:%M:%S.%3N) Sim respond signal '$1'" >> /tmp/response.out
+    sleep 0.01
+  done
 }
 
-# Trap signals and specify the corresponding handler
-for n in $(seq 1 31) ; do
-  trap "handle_signal $n" $n
+# Function to handle signals
+handle_signal() {
+  echo "$(date +%H:%M:%S.%3N) Caught signal '$1'" >> /tmp/signals.out
+  simulate_response $1 &
+}
+
+# Trap signals and specify the corresponding handler, excluding 17 SIGCHLD
+for signal in $(kill -l | grep -oh 'SIG\S*' | grep -ve '[+-]' -ve CHLD | sed -e 's/^SIG//'
+) ; do
+  trap "handle_signal $signal" $signal
 done
 
 # Keep script running to catch signals
